a644977659c885ee56b6cacbcdfe8539f67ef916
isDohyeon <eun041212@gmail.com>
2025-07-03 17:59:56 +0900

    feat: 회원가입 api 구현

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.blind_timer
-
-# db.users.insert_one({
-#     "name": "tester",
-#     "email": "test@test.com",
-#     "password": "test123"
-# })
 
 
 @app.route('/')
@@ -20,6 +14,41 @@ def home():
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
+
+@app.route('/api/users/signup', methods=['POST'])
+def signup_api():
+    name = request.form['name']
+    print(name)
+    email = request.form['email']
+    print(email)
+    password = request.form['password']
+    print(password)
+
+    if not name or not email or not password:
+        return jsonify({
+            "success": False,
+            "message": "Fail: name, email, and password are required."
+        }), 400
+
+    exsisting_user = db.users.find_one({"email": email})
+    if exsisting_user:
+        return jsonify({
+            "success": False,
+            "message": "Fail: Email already exists."
+        }), 409
+
+    signup_user = {
+        "name": name,
+        "email": email,
+        "password": password
+    }
+    db.users.insert_one(signup_user)
+
+    return jsonify({
+        "success": True,
+        "message": "Success: Signup complete!"
+    }), 201
 
 
 if __name__ == '__main__':
--- static/signup.js
+++ static/signup.js
@@ -1,19 +1,26 @@
 $(document).ready(function () {
-  $('#signup-form').submit(function (event) {
+  $("#signup-form").submit(function (event) {
     event.preventDefault();
 
-    var formData = $(this).serialize();
+    // var formData = $(this).serialize();
+
+    var formData = {
+      name: $('input[name="name"]').val(),
+      email: $('input[name="email"]').val(),
+      password: $('input[name="password"]').val(),
+    };
+
     $.ajax({
-      url: '/api/users/signup',
-      type: 'POST',
+      url: "/api/users/signup",
+      type: "POST",
       data: formData,
       success: function (response) {
         console.log(response);
-        alert('회원가입 성공');
-        window.location.href = '/';
+        alert("회원가입 성공");
+        window.location.href = "/";
       },
       error: function () {
-        alert('오류 발생');
+        alert("오류 발생");
       },
     });
   });
